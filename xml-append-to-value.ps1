8c92463c4bd3bb9100f13d305a9098a392c40496
gabe31415 <scot.gabriel@gmail.com>
2022-04-26 20:20:30 -0500

    Initial

---
+++ xml-append-to-value.ps1
@@ -0,0 +1,23 @@
+$PathProd = "C:\_home\github-temp\xml.config"
+$PathOriginal = "C:\_home\github-temp\xml.config.001.original"
+$NewDomain = "*.abc.fake"
+
+if (Test-Path -Path $PathProd) {
+   if (-not(Test-Path -Path $PathOriginal)){
+      Copy-Item $PathProd $PathOriginal
+      Write-Host "Created backup"
+   } else {
+      Write-Host "backup already exists"
+   }
+   [xml]$xml = Get-Content -Path $PathProd
+   $webDomains = $xml.SelectSingleNode("//configuration/appSettings/add[@key = 'webDomains']")
+   if ($webDomains.value.Contains($NewDomain)){
+      Write-Host "Value IS present."
+   } else {
+      Write-Host "Value NOT present in: " $webDomains.value
+      $newValue = $webDomains.value + ', ' + $NewDomain
+      Write-Host "New string value: " $newValue
+      $webDomains.SetAttribute('value', $newValue)
+      $xml.Save($PathProd)
+   }
+}
